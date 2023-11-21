0cdb1f5ed7b1ed2a8398dd87a5867659f195454d
Rajib Guha <rajibguha1996@gmail.com>
2023-11-21 23:39:08 +1100

    Sign in, Sign up, Home Page

--- src/app/sign-in/page.tsx
+++ src/app/sign-in/page.tsx
@@ -1,57 +1,40 @@
 'use client';
-import React from 'react';
+
 import { Formik, Form, Field } from 'formik';
 
-const SignInForm: React.FC = () => {
-  const handleSignIn = (values: any) => {
-    console.log('Form values:', values);
-  };
-
+export default function SignInPage() {
   return (
-    <div className="flex items-center justify-center min-h-screen bg-cover bg-center" style={{ backgroundImage: `url('https://img.freepik.com/free-photo/elevated-view-clay-pot-with-herbal-teacups-wooden-desk_23-2148186315.jpg')` }}>
-      <div className="bg-white p-8 rounded-3xl shadow-md sm:w-96 relative">
-        <div className="absolute inset-x-0 top-0 h-24 bg-cyan-950 -mt-4 flex justify-center items-center text-white font-sans text-3xl w-5/6 rounded-lg z-10 left-1/2 transform -translate-x-1/2">
-          Vendure
+    <div className="min-h-screen flex flex-col items-center justify-center bg-cover bg-center" style={{ backgroundImage: "url('https://media.istockphoto.com/id/1077026512/photo/nature-inspired-table.jpg?s=612x612&w=0&k=20&c=TthBCamXFsPZt2CaCfd2EzuzeImWiwKTUZJTMEIKFcI=')" }}>
+      <div className="bg-white bg-opacity-90 px-12 py-6 rounded-lg shadow-md relative">
+        <div className="h-24 w-5/6 bg-cyan-950 absolute -top-4 left-1/2 transform -translate-x-1/2 rounded-lg flex items-center justify-center">
+          <p className="text-white font-mono font-semibold tracking-widest text-4xl">Vendure</p>
         </div>
-        <Formik initialValues={{ username: '', password: '' }} onSubmit={handleSignIn}>
-          <Form>
-            <div className="mt-20 mb-4">
-              <Field
-                id="username"
-                name="username"
-                type="text"
-                className="block w-full rounded-md border mt-5 p-2 border-gray-300 shadow-md"
-                placeholder="Username"
-              />
+
+        <Formik
+          initialValues={{ username: '', password: '' }}
+          onSubmit={(values) => {
+            console.log(values);
+          }}
+        >
+          <Form className="w-full max-w-xl space-y-4 mt-24"> 
+            <div className="flex flex-col">
+              <Field type="text" name="username" placeholder="Username or email" className="border border-gray-300 rounded-md py-2 px-6 w-full" /> 
             </div>
-            <div className="mb-4">
-              <Field
-                id="password"
-                name="password"
-                type="password"
-                className="block w-full rounded-md border mt-1 p-2 border-gray-300 shadow-md"
-                placeholder="Password"
-              />
+            <div className="flex flex-col pb-5">
+              <Field type="password" name="password" placeholder="Password" className="border border-gray-300 rounded-md py-2 px-6 w-full" /> 
             </div>
-            <button
-              type="submit"
-              className="w-full mt-4 bg-cyan-950 text-white py-2 px-4 rounded-md hover:bg-cyan-800 transition-colors"
-            >
-              Sign In
-            </button>
+            
+            <button type="submit" className="w-full bg-cyan-950 text-white rounded-md py-2 mt-2 hover:bg-blue-600 transition duration-300">Sign In</button>
+
+            <div className="text-center pt-2">
+              <a href="#" className="text-sm text-amber-900">Forgot your Password?</a>
+            </div>
+            <div className="text-center">
+              <p className="text-sm">Don't have an account? <a href="../sign-up" className="text-amber-500 font-semibold">Sign up</a></p>
+            </div>
           </Form>
         </Formik>
-        <div className="mt-4 text-center text-amber-800">
-          <a href="#">Forgot your password?</a>
-        </div>
-        <div className="mt-2 text-center">
-          <p className="text-gray-600 inline-block mr-1">Don't have an account?</p>
-          <a href="#" className="text-amber-600 inline-block">Sign up</a>
-        </div>
       </div>
     </div>
   );
-};
-
-export default SignInForm;
-
+}
