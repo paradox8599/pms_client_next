13a6e6b075f29d94414ae5000e3eccecc89b2cd3
paradox8599 <paradox8599@outlook.com>
2023-11-27 15:34:02 +1100

    layout fix

--- src/app/sign-up/page.tsx
+++ src/app/sign-up/page.tsx
@@ -4,6 +4,7 @@ import { Formik, Form, Field } from 'formik';
 import React, { useState } from 'react';
 import { Calendar, theme, Button } from 'antd';
 import type { Dayjs } from 'dayjs';
+import Link from 'next/link';
 
 const onChange = (value: Dayjs) => {
     console.log(value.format('YYYY-MM-DD'));
@@ -23,12 +24,6 @@ export default function SignUpPage() {
 
     const { token } = theme.useToken();
 
-    const wrapperStyle: React.CSSProperties = {
-        width: 300,
-        border: `1px solid ${token.colorBorderSecondary}`,
-        borderRadius: token.borderRadiusLG,
-    };
-
 
     const handleSubmit = (values: any) => {
         console.log(values);
@@ -43,8 +38,8 @@ export default function SignUpPage() {
 
     return (
         <div className="min-h-screen flex items-center justify-center bg-cover bg-center py-24 sm:bg-no-repeat" style={{ backgroundImage: "url('https://media.istockphoto.com/id/1077026512/photo/nature-inspired-table.jpg?s=612x612&w=0&k=20&c=TthBCamXFsPZt2CaCfd2EzuzeImWiwKTUZJTMEIKFcI=')" }}>
-            <div className="bg-white bg-opacity-90 px-12 py-6 rounded-lg shadow-md relative">
-                <div className="h-24 w-5/6 bg-cyan-950 absolute -top-4 left-1/2 transform -translate-x-1/2 rounded-lg flex items-center justify-center">
+            <div className="bg-white bg-opacity-90 px-2 md:px-12 mx-2 py-6 rounded-lg shadow-md">
+                <div className="py-8 w-5/6 mx-auto bg-cyan-950 rounded-lg flex items-center justify-center -translate-y-1/2">
                     <p className="text-white font-mono font-semibold tracking-widest text-4xl">Vendure</p>
                 </div>
 
@@ -70,11 +65,11 @@ export default function SignUpPage() {
                         console.log(values);
                     }}
                 >
-                    <Form className="lg:flex mt-24 w-full">
+                    <Form className="w-full lg:flex lg:gap-8">
                         {/* Column 1: Authorization & Name */}
-                        <div className="flex flex-col space-y-6 pr-10">
+                        <div className="space-y-6">
 
-                            <div className="flex flex-col space-y-4">
+                            <div className="space-y-4">
                                 <p className="text-xl font-bold">AUTHORIZATION</p>
                                 {inputField('Username', 'username', 'text')}
                                 {inputField('Email', 'email', 'email')}
@@ -83,17 +78,17 @@ export default function SignUpPage() {
                             </div>
 
 
-                            <div className="flex flex-col space-y-4">
+                            <div className="space-y-4">
                                 <p className="text-xl font-bold">NAME</p>
                                 {inputField('First Name', 'firstName', 'text')}
                                 {inputField('Middle Name', 'middleName', 'text')}
                                 {inputField('Last Name', 'lastName', 'text')}
-                            </div>
+                            di</div>
                         </div>
 
                         {/* Column 2: Address */}
-                        <div className="flex flex-col space-y-6 pr-10">
-                            <div className="flex flex-col space-y-4">
+                        <div className="space-y-6">
+                            <div className="space-y-4">
                                 <p className="text-xl font-bold">ADDRESS</p>
                                 {inputField('Street Number', 'streetNumber', 'text')}
                                 {inputField('Street Name', 'streetName', 'text')}
@@ -106,10 +101,10 @@ export default function SignUpPage() {
 
 
                         {/* Column 3: Extra Details */}
-                        <div className="flex flex-col space-y-6">
-                            <div className="flex flex-col space-y-4">
+                        <div className="space-y-6">
+                            <div className="space-y-4">
                                 <p className="text-xl font-bold">EXTRA DETAILS</p>
-                                <div className="flex flex-col">
+                                <div className="">
                                     <label htmlFor="gender" className="text-gray-700 text-sm font-semibold mb-1">
                                         Gender (required):
                                     </label>
@@ -124,30 +119,30 @@ export default function SignUpPage() {
                             </div>
                             <div className="flex flex-col space-y-2">
                                 <p className="text-gray-700 text-sm font-semibold">Date of Birth:</p>
-                                <div style={wrapperStyle}>
-                                    <Calendar fullscreen={false} onChange={onChange} />
-                                </div>
+                                    <Calendar className='max-w-[300px]' fullscreen={false} onChange={onChange} />
                             </div>
                         </div>
                     </Form>
                 </Formik>
 
-                <div className="flex flex-col items-center space-y-2 mt-6">
-                    <label className="flex items-center space-x-2">
+                <div className="text-center space-y-2 mt-6">
+                    <label className="space-x-1">
                         <input type="checkbox" className="border-gray-300 rounded-md h-4 w-4" />
                         <span className="text-md">I agree with the terms and conditions</span>
                     </label>
 
-                    <Button
-                        type="primary"
-                        htmlType="submit"
-                        className="w-1/3"
-                    >
-                        Sign Up
-                    </Button>
+                    <div>
+                        <Button
+                            type="primary"
+                            htmlType="submit"
+                            className="w-full md:w-1/3"
+                            >
+                            Sign Up
+                        </Button>
+                    </div>
 
                     <div className="text-md">
-                        Already have an account? <a href="/sign-in" className="text-lime-500 font-semibold">Sign In</a>
+                        Already have an account? <Link href="/sign-in" className="text-lime-500 font-semibold">Sign In</Link>
                     </div>
                 </div>
             </div>
